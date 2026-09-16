98cb32bc9be09542382629f519c5c9c2d47e6deb
Willilili23 <williammontano22@gmail.com>
2026-09-15 23:20:34 -0600

    bugs2
    bugs2

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import App from "./App";
 
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// registerRootComponent llama a AppRegistry.registerComponent('main', () => App);
+// Asegura que el entorno cargue tu componente principal sin importar el método de ejecución.
 registerRootComponent(App);
